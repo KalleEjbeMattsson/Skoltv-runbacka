f2aed261c593e3c80073495934ca2739900b4865
Theodor Lindberg <theodor.lindberg@gmail.com>
2019-02-08 18:54:44 +0100

    Fixed some things and now it is time for css
    Signed-off-by: Theodor Lindberg <theodor.lindberg@gmail.com>

---
+++ skolmaten.js
@@ -0,0 +1,63 @@
+var foodList = document.getElementById("Foodlist");
+console.log(foodList);
+
+// Dagens mat och nästa mat
+async function getData() {
+    return await fetch('https://skolmaten.se/api/openmeal/v2/meals.json?distributorID=5998825718153216').then(response => response.json());
+}
+
+var weekdayMap = [
+    "Måndag", "Tisdag", "Onsdag", "Torsdag", "Fredag", "Lördag", "Söndag"
+];
+
+Date.prototype.getWeekNumber = function() {
+    var d = new Date(Date.UTC(this.getFullYear(), this.getMonth(), this.getDate()));
+    var dayNum = d.getUTCDay() || 7;
+    d.setUTCDate(d.getUTCDate() + 4 - dayNum);
+    var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
+    return Math.ceil((((d - yearStart) / 86400000) + 1) / 7)
+};
+
+getData().then(result => {
+
+    console.log(result);
+    var meals = result.data[0].meals;
+    console.log(meals);
+    var currentDate = new Date();
+    var startWeek = currentDate.getWeekNumber();
+
+    var element = document.createElement("h1");
+    element.innerHTML += "Den här veckan: " + startWeek;
+    foodList.appendChild(element);
+    var nextweek = false;
+    meals.forEach(meal => {
+        var div = document.createElement("div");
+        var date = new Date(meal.date);
+
+        if (date.getWeekNumber() != startWeek && !nextweek) {
+            var element = document.createElement("h1");
+            element.innerHTML += "Nästa vecka: " + date.getWeekNumber();
+            foodList.appendChild(element);
+            nextweek = true;
+        }
+
+        if (date.getDate() == currentDate.getDate()) {
+
+            div.innerHTML += "<p>Idag:</p>";
+        } else {
+
+            div.innerHTML += "<p>" + weekdayMap[date.getDay() - 1] + ":</p>";
+        }
+        div.innerHTML += "<ul>";
+        console.log(meal);
+        meal.courses.forEach((course) => {
+            div.innerHTML += "<li>" + course.name + "</li>";
+        });
+
+        div.innerHTML += "</ul>";
+
+        foodList.appendChild(div);
+    });
+
+
+});
